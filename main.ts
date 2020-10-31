282157e75b7a676bb2acc2eee2b2ec785641e15a
TurquoiseTNT <69688534+TurquoiseTNT@users.noreply.github.com>
2020-10-31 15:52:43 +0000

    THE REAL DEAL

--- main.ts
+++ main.ts
@@ -1,3 +1,59 @@
+devices.onNotified(MesDeviceInfo.IncomingCall, function () {
+    basic.showLeds(`
+        . # # # #
+        . # # . #
+        . . . . #
+        . # # . #
+        . # # # #
+        `)
+    music.playMelody("C5 A C5 B C5 C B A ", 125)
+    basic.showLeds(`
+        . # # # #
+        . # # . #
+        . . . . #
+        . # # . #
+        . # # # #
+        `)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . . # # .
+        # . # . #
+        . # # # .
+        # . # . #
+        . . # # .
+        `)
+    basic.clearScreen()
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        # # . # #
+        # # . # #
+        `)
+    music.playMelody("B C5 A B G A F E ", 125)
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        # # . # #
+        # # . # #
+        `)
+    basic.clearScreen()
+})
 basic.showLeds(`
     # # # # #
     # . . . .
